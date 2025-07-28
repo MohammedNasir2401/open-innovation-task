16b9e13a15efa89d911c5aac282c2486c487a822
Moh <mhdnasir44@gmail.com>
2025-07-28 14:48:08 +0400

    fix: vite config

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
 import path from 'path'
-
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   resolve: {
